9425a24cc7b6663a77a88be9e93f881930104eb7
ultjeremy <jeremykang@berkeley.edu>
2025-07-15 11:21:45 -0700

    added some comments to token_send

--- contracts/token_send.sol
+++ contracts/token_send.sol
@@ -9,13 +9,17 @@ contract tokenSend {
     // Sender does not have enough currency to send amount
     error NotEnoughCurrency(uint amount);
     
+    // Function that transferes payment from the caller to a receiver
     function sendToken(address payable receiver) external payable {
-        
+
+        // Ensure that the sender sends more than nothing
         require(msg.value > 0, "You have to send something.");
 
+        // Make sure the transfer successfully went through
         (bool success, ) = receiver.call{value: msg.value}("");
         require(success, "Transfer failed.");
 
+        // Log the transfer
         emit CurrencySent(msg.sender, receiver, msg.value);
 
         }
